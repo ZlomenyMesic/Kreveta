f615c35d5762b581b0ef1d230fe245747969f6c4
ZlomenyMesic <michna.danecek@gmail.com>
2025-04-26 23:01:14 +0200

    castling movegen bugfix

--- Kreveta/Benchmarks.cs
+++ Kreveta/Benchmarks.cs
@@ -3,6 +3,12 @@
 // started 4-3-2025
 //
 
+// Remove unnecessary suppression
+#pragma warning disable IDE0079
+
+// Mark members as static
+#pragma warning disable CA1822
+
 using BenchmarkDotNet.Attributes;
 
 namespace Kreveta;
@@ -15,4 +21,29 @@ public class Benchmarks {
     [GlobalSetup]
     public void Setup() {
     }
+
+    static int K() => 10;
+
+    [Benchmark]
+    public void INT() {
+        int x = K();
+
+        x += 128;
+        x /= 2;
+        x -= K() * 4;
+        x *= 12 - K();
+    }
+
+    [Benchmark]
+    public void BYTE() {
+        byte x = (byte)K();
+
+        x += 128;
+        x /= 2;
+        x -= (byte)(K() * 4);
+        x *= (byte)(12 - K());
+    }
 }
+
+#pragma warning restore CA1822
+#pragma warning restore IDE0079
