164308fd449930d46b01041d5899e5503317ed9a
KryKomDev <krystof.komzak@gmail.com>
2026-03-19 12:25:53 +0100

    Implement cross-platform SIMD with compile-time dispatch and fix priority access

--- Kreveta/movegen/Pext.cs
+++ Kreveta/movegen/Pext.cs
@@ -11,12 +11,27 @@ namespace Kreveta.movegen;
 
 internal static class Pext {
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    internal static ulong ParallelBitExtract(ulong val, ulong mask) {
+#if BMI2
+        return Bmi2.X64.ParallelBitExtract(val, mask);
+#else
+        ulong res = 0;
+        for (ulong bb = 1; mask != 0; bb <<= 1) {
+            if ((val & (mask & (0UL - mask))) != 0)
+                res |= bb;
+            mask &= mask - 1;
+        }
+        return res;
+#endif
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     internal static ulong GetBishopTargets(int sq, ulong free, ulong occupied) {
         // mask of relevant squares - diag/antidiag, excluding edge squares
         ulong relevantMask = PextLookupTables.BishopMask[sq];
         
         // extract relevant occupancy bits into dense index using PEXT
-        int index = (int)Bmi2.X64.ParallelBitExtract(occupied, relevantMask);
+        int index = (int)ParallelBitExtract(occupied, relevantMask);
         
         // start of the bishop attack table
         ref ulong tableStart = ref MemoryMarshal.GetArrayDataReference(PextLookupTables.FlatBishopTable);
@@ -31,7 +46,7 @@ internal static class Pext {
     internal static ulong GetRookTargets(int sq, ulong free, ulong occupied) {
         // the same goes for the rook
         ulong     relevantMask = PextLookupTables.RookMask[sq];
-        int       index        = (int)Bmi2.X64.ParallelBitExtract(occupied, relevantMask);
+        int       index        = (int)ParallelBitExtract(occupied, relevantMask);
         ref ulong tableStart   = ref MemoryMarshal.GetArrayDataReference(PextLookupTables.FlatRookTable);
         return Unsafe.Add(ref tableStart, PextLookupTables.RookOffset[sq] + index) & free;
     }
